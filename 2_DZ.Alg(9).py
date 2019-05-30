b0c0ba82fef7a6e97467289b3313465b679c5158
poly0501 <poly05012@yandex.ru>
2019-05-30 13:41:41 +0300

    Update 2_DZ.Alg(9).py
    Исправлено

--- 2_DZ.Alg(9).py
+++ 2_DZ.Alg(9).py
@@ -1,13 +1,20 @@
+print("9)Среди натуральных чисел, которые были введены, найти наибольшее по сумме цифр. Вывести на экран это число и сумму его цифр.")
 Max1 = 0
 Max2 = 0
+N = int()
+N2=0
 print("Введите число")
 X = int(input("X="))
+N = X
 while X != 0:
+    N2=X
     while X % 10 != 0:
         Max2 += X % 10
-        X=X//10
+        X = X//10
     if Max2 > Max1:
+        N = N2
         Max1 = Max2
     Max2 = 0
     X = int(input("X="))
-print("MAX =",Max1)
+print("Число:", N)
+print("MAX=", Max1)
