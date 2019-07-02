b0403a0d315e7c40e86a8103c8eaec3a6e52cef5
poly0501 <poly05012@yandex.ru>
2019-07-02 15:07:15 +0300

    Create 7_DZ.Alg(3).py
    №3

---
+++ 7_DZ.Alg(3).py
@@ -0,0 +1,33 @@
+import random
+print(" Массив размером 2m + 1, где m — натуральное число, заполнен случайным образом. Найдите в массиве медиану. Медианой называется элемент ряда, делящий его на две равные части: в одной находятся элементы, которые не меньше медианы, в другой — не больше медианы.")
+
+
+def new_sort(x, y, c):
+    if (x - y) > 0:
+        return(1)
+    if(x-y) < 0:
+        return (0)
+
+
+def _mediana_(array, m):
+    t = array[(2*m+1)//2]
+    return(t)
+
+
+MIN = 0
+MAX = 50
+c = 0
+n = 1
+m = int(input('Введите m = '))
+array = [random.randint(MIN, MAX) for _ in range(2*m+1)]
+print("Длина списка:", 2*m+1)
+print("Исходный список\n", array)
+while n == 1:
+    n = 0
+    for i in range(2*m):
+        if new_sort(array[i], array[i+1], c) == 1:
+            array[i], array[i + 1] = array[i + 1], array[i]
+            n = 1
+print("Отсортированный список:\n", array)
+print("\nМедиана нашего списка:", _mediana_(array, m))
+
